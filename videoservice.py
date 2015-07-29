d63e68282a823dff2660f9b894bf56006163497f
Niall Robinson <niall.h.robinson@gmail.com>
2015-07-29 12:12:09 +0100

    queing stuff

--- videoservice.py
+++ videoservice.py
@@ -1,67 +1,78 @@
 #!/usr/bin/env python
 
-import time
 import argparse as ap
 import config
 import restnavigator as rn
-import logging
-import logging.handlers
 import tempfile
 import urllib
 import os
 import shutil
+import sys
+
+class Job(object):
+    def __init__(self, message):
+        self.profile = message["profile"]
+        self.model = message["model"]
+        self.variable = message["variable"]
+        self.message = message
+
+    def __str__(self):
+        return __dict__
+
+    def __repr__(self):
+        return __dict__
 
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-handler = logging.handlers.RotatingFileHandler('imageservice.log', maxBytes=1e6, backupCount=3)
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+def getQueue(queue_name):
+    conn = boto.sqs.connect_to_region(os.getenv("AWS_REGION"),
+                                      aws_access_key_id=os.getenv("AWS_KEY"),
+                                      aws_secret_access_key=os.getenv("AWS_SECRET_KEY"))
+    queue = conn.get_queue(queue_name)
+    return queue
+
+
+def getJob(queue, visibility_timeout=60):
+    messages = queue.get_messages(visibility_timeout, number_messages=1) #get all messages
+    try:
+        message = messages[0]
+    except IndexError:
+        raise NoJobsError()
+
+    return Job(message) # NOT YET IMPLIMETNED
+
 
 if __name__ == "__main__":
-    argparser = ap.ArgumentParser()
-    argparser.add_argument("-s", "--settings", default="default",
-        type=str, help="Name of analysis settings, as defined in config.py")
-    call_args = argparser.parse_args()
-    settings = ap.Namespace(**config.settings[call_args.settings])
+    video_service_queue = getQueue("video_service_queue")
+    job = getJob(video_service_queue)
 
+    settings = ap.Namespace(**config.profile[job.profile])
     rootnav = rn.Navigator.hal(config.roothal)
 
-    while True:
-        try: # catch all try except so that service keeps running
-            for variable in settings.variables:
-                try:
-                    varnav = rootnav["models"][0][settings.model][0]["latest"][0][variable][0]
-                except rn.exc.OffTheRailsException:
-                    logger.exception(rn.exc.OffTheRailsException("Variable " + variable + "not found."))
-                else:
-                    imgnav = varnav["images"]
-                    if ("videos" not in varnav.links().keys()) and (len(imgnav) == settings.nframes):
-                        logger.info("Making video for " \
-                                    + imgnav[0].fetch()["forecast_reference_time"] \
-                                    + " " + variable)
-                        tempdir = tempfile.mkdtemp()
-                        for thisimgnav in imgnav:
-                            img = thisimgnav[0].embedded()["images"]
-                            imgmetadata = thisimgnav.fetch()
-                            urllib.urlretrieve(img.uri,
-                                    os.path.join([tmpdir, imgmetadata['forecast_reference_time']+".png"]))
-                            with tempfile.SpooledTemporaryFile(max_size=2e7, suffix=settings.video_ending) as vid:
-                                # wrapping the wildcard in single quotes below means that it is not exanded by the shell
-                                args = settings.ffmpeg_args_template
-                                args[args.index("FILES_IN")] = "'"+os.path.join([tmpdir, "*.png"]+"'")
-                                args[args.index("FILE_OUT")] = vid
-                                logger.info("Calling ffmpeg with these args: ", args)
-                                os.call(args)
+    try:
+        varnav = rootnav["models"][job.model]["latest"][job.variable]
+    except rn.exc.OffTheRailsException:
+        raise rn.exc.OffTheRailsException("Variable " + job.variable + "not found."))
+    else:
+        imgnav = varnav["images"]
+        tempdir = tempfile.mkdtemp()
+        for thisimgnav in imgnav:
+            img = thisimgnav.embedded()["images"]
+            imgmetadata = thisimgnav.fetch()
+            urllib.urlretrieve(img.uri,
+                    os.path.join([tmpdir, imgmetadata['forecast_reference_time']+".png"]))
+            with tempfile.SpooledTemporaryFile(max_size=2e7, suffix=settings.video_ending) as vid:
+                # wrapping the wildcard in single quotes below means that it is not exanded by the shell
+                args = settings.ffmpeg_args_template
+                args[args.index("FILES_IN")] = "'"+os.path.join([tmpdir, "*.png"]+"'")
+                args[args.index("FILE_OUT")] = vid
+                os.call(args)
 
-                                payload = imgmetadata.pop("forecast_time")
-                                r = requests.post(config.vid_dest, data=payload, files={"data": vid})
-                                if r.status_code != 201:
-                                    raise IOError(r.status_code, r.text)
-                                logger.info("Video posted")
-                        shutil.rmtree(tempdir)
-        except BaseException as e:
-            logger.exception(e)
-        time.sleep(60)
+                payload = imgmetadata.pop("forecast_time")
+                r = requests.post(config.vid_dest, data=payload, files={"data": vid})
+                if r.status_code != 201:
+                    raise IOError(r.status_code, r.text)
+        shutil.rmtree(tempdir)
+
+        video_service_queue.delete(job.message)
+
+        sys.exit()
